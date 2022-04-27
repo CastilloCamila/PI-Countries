6175ba70fc94ad18e20b7dfc72cc49c9d5db64fc
Castillo Camila <camila.castillo.manrique@gmail.com>
2022-04-27 00:30:13 -0300

    Remove the name requirement

--- api/src/routes/activities.js
+++ api/src/routes/activities.js
@@ -6,16 +6,16 @@ const router = Router()
 router.post('/', async (req, res, next) => {
     try {
         const { name, difficulty, duration, season, countries } = req.body;
-        if (name) {
+        
             const propiedades = { name, difficulty, duration, season }
             let activity = await Activity.findOrCreate({
                 where: propiedades
             ,
                 default: propiedades
         })
-            // activity.setCountries(countries)
+            activity.setCountries(countries)
             return res.json(activity)
-        } else return res.status(400).json({ msg: 'A name is needed' })
+       
     } catch (error) {
         next(error)
     }
