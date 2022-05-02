e73f7bfd1677c22843c356180fb12958ddf2def4
Castillo Camila <camila.castillo.manrique@gmail.com>
2022-05-02 00:26:14 -0300

    Functional search

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,30 @@
+import {useState} from 'react'
+import { useDispatch } from 'react-redux'
+import { searchCountry } from '../redux/actions'
+
+export default function Search() {
+    const dispatch = useDispatch()
+    let [search,setSearch]=useState(
+        {country:''}
+    )
+
+    function handleOnChange(e){
+        setSearch(
+            {
+                ...search,
+                country:e.target.value
+            }
+        )
+    }
+    function handleOnsubmit(event){
+        event.preventDefault()
+        console.log('submitt')
+        dispatch(searchCountry(search.country))
+    }
+    return(
+        <div>
+            <input type="text" name="search" value={search.search} onChange={handleOnChange} />
+            <button onClick={handleOnsubmit}>Buscar</button>
+        </div>
+    )
+}
