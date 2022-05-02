e2eb0915d835de22534844ba21375e790d4d8c5b
Castillo Camila <camila.castillo.manrique@gmail.com>
2022-05-02 17:18:29 -0300

    Paginated

--- client/src/components/home/AllCards.jsx
+++ client/src/components/home/AllCards.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux'
 import CardCountry from './CardCountry'
 import {getAllCountries} from '../../redux/actions/index.js'
@@ -6,21 +6,43 @@ import {getAllCountries} from '../../redux/actions/index.js'
 import { Link } from "react-router-dom";
 
 export default function AllCards(props) {
+    let [currentPage, setcurrentPage] = useState(0)
     const dispatch = useDispatch()
     const countries = useSelector((state) => state.countries)
     useEffect(() => {
         dispatch(getAllCountries())
     },[dispatch])
 
+    function filterCountries() {
+        let filteredCountries=null
+        if(currentPage==0){
+         filteredCountries= countries.slice(currentPage,currentPage+9)}
+        else { filteredCountries= countries.slice(currentPage,currentPage+10)}
+        
+  
+        return filteredCountries
+    }
+    function nextPage() {
+        setcurrentPage((currentPage-1) + 10)
+    }
+    function previusPage() {
+        if(currentPage>0)setcurrentPage((currentPage+1) - 10)
+    }
     return (
         <>
         <div>
             <Link to='/home/activity'>
                 <p>actividades</p>
             </Link>
+            <button onClick={()=>nextPage()}>
+                Next
+            </button>
+            <button onClick={()=>previusPage()}>
+                prev
+            </button>
             
             {
-                countries.length >0 ?  countries.map((country)=>(
+                countries.length >0 ?  filterCountries().map((country)=>(
                     <>
                    
                     <CardCountry name={country.name} image={country.image} continent={country.continent} id={country.id} key={country.id}/>
