f23179680f64db75642194a9d297f55d03fb13b0
Castillo Camila <camila.castillo.manrique@gmail.com>
2022-05-13 13:11:22 -0300

    Add key and id

--- client/src/components/home/countries/countryDetail/countryDetail/CountryDetail.jsx
+++ client/src/components/home/countries/countryDetail/countryDetail/CountryDetail.jsx
@@ -29,56 +29,56 @@ export default function CountryDetail() {
 
     return (<>
 
-
-        {countryDetail.id ?
+       
+            {countryDetail.id ?
             <>
-                <div className={style.card}>
-                    <div className={style.cardDetail}>
-                        <h1>{countryDetail.name}</h1>
-                        <img src={`${countryDetail.image}`} alt="" />
-                        <p>Continent: {countryDetail.continent}</p>
-                        <p>Capital: {countryDetail.capital}</p>
-                        <p>Subregion: {countryDetail.subregion}</p>
-                        <p>Area: {countryDetail.area}</p>
-                        <p>Papulation: {countryDetail.population}</p>
+            <div className={style.card}>
+                <div className={style.cardDetail}>
+                    <h1>{countryDetail.name}</h1>
+                    <img src={`${countryDetail.image}`} alt="" />
+                    <p>Continent: {countryDetail.continent}</p>
+                    <p>Capital: {countryDetail.capital}</p>
+                    <p>Subregion: {countryDetail.subregion}</p>
+                    <p>Area: {countryDetail.area}</p>
+                    <p>Papulation: {countryDetail.population}</p>
 
 
-                    </div>
-                    <div className={style.activityDetail}>
-                        <p>  Activities Of This Country</p>
-                        <div className={style.scroll}>
-                            {countryDetail.activities.length > 0 ?
+                </div>
+                <div className={style.activityDetail}>
+                    <p>  Activities Of This Country</p>
+                    <div className={style.scroll}>
+                        { countryDetail.activities.length >0?
+                        
 
-
-                                countryDetail.activities.map((actDetail, number = 0) => {
-                                    number++
-                                    return <AcivityDetail name={actDetail.name} difficulty={actDetail.difficulty} duration={actDetail.duration} season={actDetail.season} number={number} id={actDetail.id} key={actDetail.id} />
-                                })
-                                :
+                            countryDetail.activities.map((actDetail, number = 0) => {
+                                number++
+                                return <AcivityDetail name={actDetail.name} difficulty={actDetail.difficulty} duration={actDetail.duration} season={actDetail.season} number={number} id={actDetail.id} key={actDetail.id} />
+                            })
+                                : 
                                 <>
-                                    <img src={imgLoading2} alt="" />
-                                    <p>
-                                        There are no activities for this country.</p>
-                                    <p> If you want you can add one in "Add Activity"</p>
+                                <img src={imgLoading2} alt="" />
+                                <p>
+                                There are no activities for this country.</p>
+                               <p> If you want you can add one in "Add Activity"</p>
                                 </>
 
-                            }
+                        } 
 
 
-                        </div>
                     </div>
                 </div>
+                </div>
             </>
-            :
+            : 
             <>
                 <div className={style.loading}>
                     <img src={imgLoading} alt="" />
                     <h1>Cargando</h1>
                 </div>
             </>
+             
 
-
-        }
-    </>
+        } 
+       </> 
     )
 }
