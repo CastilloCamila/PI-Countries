7668d876458f469b63289db4c69749dc173a4a8b
CastilloCamila <camila.castillo.manrique@gmail.com>
2022-06-24 03:48:39 -0300

    Last Fix

--- client/src/components/home/filters/filters/Filters.jsx
+++ client/src/components/home/filters/filters/Filters.jsx
@@ -29,7 +29,9 @@ export default function Filters() {
     
     //------------------------------
 
-
+    useEffect(()=>{
+        dispatch(getAllActivities())
+    },[dispatch])
 
     useEffect(() => {
 
@@ -98,6 +100,7 @@ export default function Filters() {
             }
         }
         if (activity !== '') {
+            dispatch(getAllActivities())
             
             const filterActivies = allActivities.find(act => act.id === activity)
             dispatch(filtered(filterActivies.countries))
@@ -193,52 +196,6 @@ export default function Filters() {
                     </Menu>
                 </ProSidebar>
             
-            {/* <div className={style.conteinerFilters}>
-            <div className={`${style.divsConteiners} ${style.label} ${style.select}`}>
-                <label htmlFor="continent">Filter By Continent:</label>
-                <select name="continent" onChange={e => setContinent(e.target.value)} id="">
-                    <option value="">Select a Continent</option>
-                    <option value="Africa">Africa</option>
-                    <option value="Asia">Asia</option>
-                    <option value="North America">North America</option>
-                    <option value="South America">South America</option>
-                    <option value="Antarctica">Antarctica</option>
-                    <option value="Europe">Europe</option>
-                    <option value="Oceania">Oceania</option>
-
-                </select>
-            </div>
-            <div className={style.divsConteiners}>
-                <label htmlFor="population">Filter By Population:</label>
-                <select name="population" onChange={e => setPopulation(e.target.value)} id="">
-                    <option value="">Order...</option>
-                    <option value="asc">Ascending</option>
-                    <option value="desc">Descending</option>
-                </select>
-            </div>
-            <div className={style.divsConteiners}>
-                <label htmlFor="alphabetical">Filter By Alphabetical:</label>
-                <select name="alphabetical" onChange={e => setAlphabetical(e.target.value)} id="">
-                    <option value="">Order...</option>
-                    <option value="asc">Ascending</option>
-                    <option value="desc">Descending</option>
-                </select>
-            </div>
-            <div className={style.divsConteiners}>
-                <label htmlFor="activity">Filter By Activity:</label>
-                <select name="activity" id='laAC' value={activity} onChange={(e) => setActivity(e.target.value)}>
-                    <option value="">Select an Activity</option>
-                    {
-                        allActivities.map((act) => {
-
-                            return <option value={act.id}>{act.name}</option>
-                        })
-
-                    }
-                </select>
-            </div>
-            <button className={style.button} onClick={() => reset()}>RESET FILTERS</button>
-        </div> */}
         </>
 
     )
